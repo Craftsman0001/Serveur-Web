2eeae6f59f867c04aa72c1abf0aabf4e66186570
Craftsman0001 <131674449+Craftsman0001@users.noreply.github.com>
2025-03-24 15:22:49 +0100

    modification de webserver

--- src/WebServer.java
+++ src/WebServer.java
@@ -6,27 +6,6 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class WebServer {
-    private void readRequest(Socket socket){ 
-        try {
-            BufferedReader input;
-            input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            String response = "";
-            while((response = input.readLine()).isEmpty() == false){
-                System.out.println(response);
-            }
-        }catch(Exception e){
-            System.err.println(e.getMessage());
-        }
-    }
-    private void sendResponse(Socket socket){
-        try{
-            BufferedWriter output = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-            output.write("HTTP/1.0 200 OK\n");
-            output.flush();
-        }catch(Exception e){
-            System.err.println(e.getMessage());
-        }
-    }
     public WebServer(){}
     public void run(int portNumber){
         try{
@@ -34,8 +13,7 @@ public class WebServer {
             while(true){
                 Socket clienSocket = serverSocket.accept();
                 //traitement de la requete client
-                readRequest(clienSocket);
-                sendResponse(clienSocket);
+                RequestProcessor requestProcessor = new RequestProcessor(clienSocket);
                 clienSocket.close();
             }
         }catch(Exception e){
