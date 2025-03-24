0035672c19c6e1ea9cdcae3ffdad88f8df481dd7
Craftsman0001 <131674449+Craftsman0001@users.noreply.github.com>
2025-03-24 14:07:12 +0100

    bon fonctionnement de webserveurapplication et webserver : page blanche à l'adresse localhost

--- src/WebServer.java
+++ src/WebServer.java
@@ -1,12 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class WebServer {
 private void readRequest(Socket socket){
-    
+    BufferedReader input;
+    try {
+        input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        String response = "";
+        while((response = input.readLine()).isEmpty() == false)
+        System.out.println(response);
+    }catch(Exception e){
+        System.err.println(e.getMessage());
+    }
 }
 private void sendResponse(Socket socket){
-
+    try{
+        BufferedWriter output = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        output.write("HTTP/1.0 200 OK\n");
+        output.flush();
+    }catch(Exception e){
+        System.err.println(e.getMessage());
+    }
 }
 public WebServer(){}
 public void run(int portNumber){
